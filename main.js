d28fdeeae3ec4854efdf9e3bc4bff737848d3f74
Ananya2010Coding <85874739+Ananya2010Coding@users.noreply.github.com>
2021-11-11 19:28:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,14 @@ function setup(){
 function draw(){
     image(video , 0 , 0 , 640 , 480)
 }
+
+function start(){
+    objectDetection = ml5.objectDetector('cocossd',modelLoaded);
+    object_name = document.getElementById("input").value;
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model has Loaded!!");
+    Status = true;
+}
